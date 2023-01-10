b194eeba10925d9c283f56661a3d85f150685efb
papow65 <papow65@protonmail.com>
2023-01-10 02:30:25 +0100

    Made manage_characters a world-system

--- src/systems/character.rs
+++ src/systems/character.rs
@@ -1,6 +1,6 @@
 use super::log_if_slow;
 use crate::prelude::*;
-use bevy::prelude::*;
+use bevy::{ecs::system::SystemState, prelude::*};
 use std::time::Instant;
 
 #[allow(clippy::needless_pass_by_value)]
@@ -18,70 +18,83 @@ pub(crate) fn manage_game_over(
 }
 
 #[allow(clippy::needless_pass_by_value)]
-pub(crate) fn manage_characters(
-    mut commands: Commands,
-    mut envir: Envir,
-    mut instruction_queue: ResMut<InstructionQueue>,
-    mut timeouts: ResMut<Timeouts>,
-    actors: Actors,
-    mut players: Query<&mut Player>,
-    dumpees: Query<(Entity, &Label, &Parent)>,
-    hierarchy: Hierarchy, // pickup
-) {
+pub(crate) fn manage_characters(world: &mut World) {
     let start = Instant::now();
 
+    let mut system_state: SystemState<(
+        Commands,
+        Envir,
+        ResMut<InstructionQueue>,
+        ResMut<Timeouts>,
+        Actors,
+        Query<&mut Player>,
+        Query<(Entity, &Label, &Parent)>,
+        Hierarchy, // pickup
+    )> = SystemState::new(world);
+
     while start.elapsed() < MAX_SYSTEM_DURATION / 2 {
-        let entities = actors
+        let (
+            mut commands,
+            mut envir,
+            mut instruction_queue,
+            mut timeouts,
+            actors,
+            mut players,
+            dumpees,
+            hierarchy,
+        ) = system_state.get_mut(world);
+
+        let egible_entities = actors
             .actors()
             .filter(|a| envir.has_floor(a.pos) || players.get(a.entity).is_ok())
             .map(|a| a.entity)
             .collect::<Vec<Entity>>();
-        if let Some(active_entity) = timeouts.next(&entities) {
-            let factions = actors.collect_factions();
-            let actor = Actor::from(actors.q.get(active_entity).unwrap());
-            let action = if let Ok(ref mut player) = players.get_mut(active_entity) {
-                if let Some(action) = player.plan_action(
-                    &mut commands,
-                    &mut envir,
-                    &mut instruction_queue,
-                    actor.pos,
-                    timeouts.time(),
-                ) {
-                    action
-                } else if let PlayerActionState::Waiting(until) = player.state {
-                    if until <= timeouts.time() {
-                        instruction_queue.add(QueuedInstruction::Cancel);
-                        break; // process the cancellation next turn
-                    } else {
-                        Action::Stay
-                    }
+        let Some(active_entity) = timeouts.next(&egible_entities) else {
+            eprintln!("No egible characters!");
+            return;
+        };
+        let factions = actors.collect_factions();
+        let actor = Actor::from(actors.q.get(active_entity).unwrap());
+        let action = if let Ok(ref mut player) = players.get_mut(active_entity) {
+            if let Some(action) = player.plan_action(
+                &mut commands,
+                &mut envir,
+                &mut instruction_queue,
+                actor.pos,
+                timeouts.time(),
+            ) {
+                action
+            } else if let PlayerActionState::Waiting(until) = player.state {
+                if until <= timeouts.time() {
+                    instruction_queue.add(QueuedInstruction::Cancel);
+                    break; // process the cancellation next turn
                 } else {
-                    break; // no key pressed - wait for the user
+                    Action::Stay
                 }
             } else {
-                let strategy = actor.faction.strategize(&envir, &factions, &actor);
-                if let Some(last_enemy) = strategy.last_enemy {
-                    commands.entity(actor.entity).insert(last_enemy);
-                }
-                println!(
-                    "{} at {:?} plans {:?} and does {:?}",
-                    actor.label, actor.pos, strategy.intent, strategy.action
-                );
-                strategy.action
-            };
-            let mut timeout =
-                actor.perform(&mut commands, &mut envir, &dumpees, &hierarchy, &action);
-
-            if timeout.0 == 0 && players.get(active_entity).is_err() {
-                commands.spawn(Message::error("failed npc action"));
-                timeout.0 = 1000;
+                break; // no key pressed - wait for the user
             }
-
-            timeouts.add(active_entity, timeout);
         } else {
-            // No characters!
-            break;
+            let strategy = actor.faction.strategize(&envir, &factions, &actor);
+            if let Some(last_enemy) = strategy.last_enemy {
+                commands.entity(actor.entity).insert(last_enemy);
+            }
+            println!(
+                "{} at {:?} plans {:?} and does {:?}",
+                actor.label, actor.pos, strategy.intent, strategy.action
+            );
+            strategy.action
+        };
+        let mut timeout = actor.perform(&mut commands, &mut envir, &dumpees, &hierarchy, &action);
+
+        if timeout.0 == 0 && players.get(active_entity).is_err() {
+            commands.spawn(Message::error("failed npc action"));
+            timeout.0 = 1000;
         }
+
+        timeouts.add(active_entity, timeout);
+
+        system_state.apply(world);
     }
 
     log_if_slow("manage_characters", start);
