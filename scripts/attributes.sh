352c9db9e34ec4e07b869a6d2138e0ed05fc49d8
papow65 <papow65@protonmail.com>
2023-04-10 07:02:15 +0200

    Added support for subtypes

--- scripts/attributes.sh
+++ scripts/attributes.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 for F in $(find assets/data/json/ -type f) ; do
-    for LINE in $(jq -r '.[] | [(keys | join(" ") | sub("~"; "(tilde)")), .type] | @csv' $F | sed -e 's#"##g' -e 's# #~#g') ; do
+    for LINE in $(jq -r '.[] | [(keys | join(" ") | sub("~"; "(tilde)")), .type, .subtype] | @csv' $F | sed -e 's#"##g' -e 's# #~#g') ; do
         #echo -e "$LINE"
-        ATTRIBUTES=${LINE%%,*}
-        TYPE=${LINE##*,}
-        echo "$ATTRIBUTES@$TYPE" | sed -E -e "s#~#@$TYPE\n#g"
+        readarray -d, -t arr <<<"$LINE,"
+        ATTRIBUTES="${arr[0]}"
+        TYPE="${arr[1]}"
+        SUBTYPE="${arr[2]}"
+        #echo "$ATTRIBUTES@${TYPE}@${SUBTYPE}"
+        echo "$ATTRIBUTES@$TYPE@$SUBTYPE" | sed -E -e "s#~#@${TYPE}@${SUBTYPE}\n#g"
     done
 done
