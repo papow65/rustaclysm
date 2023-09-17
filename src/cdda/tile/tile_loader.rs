ac5f849ffb23314832037ab76900f580e9638860
papow65 <papow65@protonmail.com>
2023-09-17 18:36:34 +0200

    Added error message for missing model

--- src/cdda/tile/tile_loader.rs
+++ src/cdda/tile/tile_loader.rs
@@ -246,7 +246,7 @@ impl TileLoader {
 
         let foreground_model = self
             .to_model(foreground, definition, SpriteLayer::Front)
-            .unwrap();
+            .expect(format!("Foreground present for {foreground:?} with {definition:?}").as_str());
         let background_model = self.to_model(background, definition, SpriteLayer::Back);
 
         if let Some(background_model) = background_model {
