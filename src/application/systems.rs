caf8bcec36a208ab5f91f65c61706b7623e41644
papow65 <papow65@protonmail.com>
2024-03-24 14:51:44 +0100

    Corrected scrolling for padding on scrolling list parent nodes

--- src/application/systems.rs
+++ src/application/systems.rs
@@ -59,14 +59,15 @@ pub(super) fn manage_global_keyboard_input(
 pub(super) fn manage_scrolling(
     mut mouse_wheel_events: EventReader<MouseWheel>,
     mut scrolling_lists: Query<(&mut ScrollingList, &mut Style, &Parent, &Node)>,
-    nodes: Query<&Node>,
+    parent_nodes: Query<(&Node, &Style), Without<ScrollingList>>,
 ) {
     for mouse_wheel_event in mouse_wheel_events.read() {
         for (mut scrolling_list, mut style, parent, list_node) in &mut scrolling_lists {
-            let parent_node = nodes
+            let (parent_node, parent_style) = parent_nodes
                 .get(parent.get())
                 .expect("Parent node should be found");
-            style.top = scrolling_list.scroll(list_node, parent_node, mouse_wheel_event);
+            style.top =
+                scrolling_list.scroll(list_node, parent_node, parent_style, mouse_wheel_event);
         }
     }
 }
@@ -74,12 +75,12 @@ pub(super) fn manage_scrolling(
 #[allow(clippy::needless_pass_by_value)]
 pub(crate) fn resize_scrolling_lists(
     mut scrolling_lists: Query<(&mut ScrollingList, &mut Style, &Parent, &Node)>,
-    nodes: Query<&Node>,
+    parent_nodes: Query<(&Node, &Style), Without<ScrollingList>>,
 ) {
     for (mut scrolling_list, mut style, parent, list_node) in &mut scrolling_lists {
-        let parent_node = nodes
+        let (parent_node, parent_style) = parent_nodes
             .get(parent.get())
             .expect("Parent node should be found");
-        style.top = scrolling_list.resize(list_node, parent_node);
+        style.top = scrolling_list.resize(list_node, parent_node, parent_style);
     }
 }
--- src/common/scrolling_list.rs
+++ src/common/scrolling_list.rs
@@ -1,6 +1,6 @@
 use bevy::{
     input::mouse::{MouseScrollUnit, MouseWheel},
-    prelude::{Component, Node, Val},
+    prelude::{Component, Node, Style, Val},
 };
 
 #[derive(Component, Default)]
@@ -15,27 +15,51 @@ impl ScrollingList {
         &mut self,
         my_node: &Node,
         parent_node: &Node,
+        parent_style: &Style,
         mouse_wheel_event: &MouseWheel,
     ) -> Val {
         let dy = match mouse_wheel_event.unit {
             MouseScrollUnit::Line => mouse_wheel_event.y * 20.,
             MouseScrollUnit::Pixel => mouse_wheel_event.y,
         };
-        self.adjust(my_node, parent_node, dy)
+        self.adjust(my_node, parent_node, parent_style, dy)
     }
 
     #[must_use]
-    pub(crate) fn resize(&mut self, my_node: &Node, parent_node: &Node) -> Val {
-        self.adjust(my_node, parent_node, 0.0)
+    pub(crate) fn resize(
+        &mut self,
+        my_node: &Node,
+        parent_node: &Node,
+        parent_style: &Style,
+    ) -> Val {
+        self.adjust(my_node, parent_node, parent_style, 0.0)
     }
 
     #[must_use]
-    pub(crate) fn adjust(&mut self, my_node: &Node, parent_node: &Node, dy: f32) -> Val {
-        let items_height = my_node.size().y;
+    pub(crate) fn adjust(
+        &mut self,
+        my_node: &Node,
+        parent_node: &Node,
+        parent_style: &Style,
+        dy: f32,
+    ) -> Val {
+        let padding_top = Self::to_px(parent_style.padding.top);
+        let padding_bottom = Self::to_px(parent_style.padding.bottom);
+
+        let items_height = my_node.size().y + padding_top + padding_bottom;
         let parent_height = parent_node.size().y;
         let max_scroll = (items_height - parent_height).max(0.);
         self.position += dy;
         self.position = self.position.clamp(-max_scroll, 0.);
         Val::Px(self.position)
+    }
+
+    /// This assumes [`Val::Auto`] is used vertically
+    fn to_px(val: Val) -> f32 {
+        match val {
+            Val::Auto => 0.0,
+            Val::Px(px) => px,
+            other => unimplemented!("Conversion of {:?} to pixels", other),
+        }
     }
 }
