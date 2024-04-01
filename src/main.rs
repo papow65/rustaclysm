b4b00d3e4ccb95173324a990929de5d6576e554d
papow65 <papow65@protonmail.com>
2024-04-01 21:56:55 +0200

    Set RUST_BACKTRACE to '1', if it isn't set.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,13 @@ mod main_menu;
 mod prelude;
 
 use crate::prelude::run_application;
+use std::env;
 
 fn main() {
+    let rust_backtrace = "RUST_BACKTRACE";
+    if env::var_os(rust_backtrace).is_none() {
+        env::set_var(rust_backtrace, "1");
+    }
+
     run_application();
 }
