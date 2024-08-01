4fb8de7aaa6f77f6b5b57c2bf0eec7134042b587
papow65 <papow65@protonmail.com>
2024-08-01 18:01:42 +0200

    Ignored some warnings for 'cargo check --release'

--- src/common/log_transition.rs
+++ src/common/log_transition.rs
@@ -1,8 +1,10 @@
+#[allow(unused_imports)]
 use bevy::{
     dev_tools::states::log_transitions,
     prelude::{App, States, Update},
 };
 
+#[allow(unused_variables)]
 pub(crate) fn log_transition_plugin<S: States>(app: &mut App) {
     #[cfg(debug_assertions)]
     app.add_systems(Update, log_transitions::<S>);
