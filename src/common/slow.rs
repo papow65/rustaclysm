4fbaea88c8b048ea3bc63df04ba80dab926faeac
papow65 <papow65@protonmail.com>
2024-08-03 20:26:20 +0200

    Only logging slow functions when using debug_assertions

--- src/common/slow.rs
+++ src/common/slow.rs
@@ -1,12 +1,14 @@
 use std::time::{Duration, Instant};
 
-pub(crate) const MAX_SYSTEM_DURATION: Duration = Duration::from_micros(300);
+const MAX_SYSTEM_DURATION: Duration = Duration::from_micros(300);
 
 pub(crate) fn log_if_slow(name: &str, start: Instant) {
-    let duration = start.elapsed();
-    if 5 * MAX_SYSTEM_DURATION < duration {
-        eprintln!("Very slow system: {name} took {duration:?}");
-    } else if MAX_SYSTEM_DURATION < duration {
-        println!("Slow system: {name} took {duration:?}");
+    if cfg!(debug_assertions) {
+        let duration = start.elapsed();
+        if 5 * MAX_SYSTEM_DURATION < duration {
+            eprintln!("Very slow system: {name} took {duration:?}");
+        } else if MAX_SYSTEM_DURATION < duration {
+            println!("Slow system: {name} took {duration:?}");
+        }
     }
 }
