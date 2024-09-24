609941efa2535ec25b21bd1f56a109b19e0d6bed
papow65 <papow65@protonmail.com>
2024-09-25 01:39:46 +0200

    Replaced a messy unit test for tile_config.json by two integration tests

--- crates/cdda_json_files/src/tile/tile_config.rs
+++ crates/cdda_json_files/src/tile/tile_config.rs
@@ -10,18 +10,3 @@ pub struct CddaTileConfig {
     #[serde(rename = "tiles-new")]
     pub atlases: Vec<CddaAtlas>,
 }
-
-#[cfg(test)]
-mod recipe_tests {
-    use super::*;
-    #[test]
-    #[ignore] // Not a proper unit test, because the config tile may not exist
-    fn it_works() {
-        let json = include_str!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/../../assets/gfx/UltimateCataclysm/tile_config.json"
-        ));
-        let result = serde_json::from_str::<CddaTileConfig>(json);
-        assert!(result.is_ok(), "{result:?}");
-    }
-}
---
+++ crates/cdda_json_files/tests/tile_config.rs
@@ -0,0 +1,29 @@
+use cdda_json_files::CddaTileConfig;
+use reqwest::blocking::get as get_request;
+use std::fs::read;
+
+fn load(bytes: &[u8]) {
+    let result = serde_json::from_slice::<CddaTileConfig>(bytes);
+    assert!(result.is_ok(), "{result:?}");
+}
+
+#[test]
+fn from_github() {
+    let github_url = "https://raw.githubusercontent.com/CleverRaven/Cataclysm-DDA/293c7615db5b1988916cc3120cb1cf398246b92b/gfx/UltimateCataclysm/tile_config.json";
+    let github_response_body = get_request(github_url)
+        .expect("Github request should work")
+        .bytes()
+        .expect("Github response should be complete");
+    load(&github_response_body);
+}
+
+#[test]
+fn local_file_if_exists() {
+    let file_path = "../../assets/gfx/UltimateCataclysm/tile_config.json";
+    // Uncomment to check the path when the file is in place:
+    //read(file_path).expect("The path should be valid");
+
+    if let Ok(file_contents) = read(file_path) {
+        load(&file_contents);
+    }
+}
