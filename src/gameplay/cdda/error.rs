b28426375c404c2f5c1531ce5bc69937955ac984
papow65 <papow65@protonmail.com>
2024-09-25 03:13:56 +0200

    Added comment

--- src/gameplay/cdda/error.rs
+++ src/gameplay/cdda/error.rs
@@ -19,6 +19,7 @@ pub(crate) enum Error {
     },
 }
 
+// Requirement for StdError
 impl Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{self:#?}")
