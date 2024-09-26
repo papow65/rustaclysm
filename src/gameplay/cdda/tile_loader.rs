1a2d3522d4aff7a1a2b24fe624278559e35892f9
papow65 <papow65@protonmail.com>
2024-09-27 00:21:32 +0200

    Removed duplicate of old test

--- src/gameplay/cdda/tile_loader.rs
+++ src/gameplay/cdda/tile_loader.rs
@@ -152,18 +152,3 @@ impl AsyncNew<Self> for TileLoader {
         async move { Self::try_new().expect("Tiles should load") }.await
     }
 }
-
-#[cfg(test)]
-mod recipe_tests {
-    use super::*;
-    #[test]
-    #[ignore] // Not a proper unit test, because the config tile may not exist
-    fn it_works() {
-        let json = include_str!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/assets/gfx/UltimateCataclysm/tile_config.json"
-        ));
-        let result = serde_json::from_str::<CddaTileConfig>(json);
-        assert!(result.is_ok(), "{result:?}");
-    }
-}
