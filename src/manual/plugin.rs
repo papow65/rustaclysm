eada0cd8cdc68a4ace598ba463b9d05b47c3f6b9
papow65 <papow65@protonmail.com>
2024-09-28 22:59:14 +0200

    Only updating the manual when needed

--- src/manual/plugin.rs
+++ src/manual/plugin.rs
@@ -1,16 +1,25 @@
+use crate::gameplay::GameplayScreenState;
 use crate::manual::input::create_manual_key_bindings;
 use crate::manual::systems::{spawn_manual, update_manual};
-use bevy::prelude::{App, Plugin, PostStartup, Update};
+use bevy::prelude::{
+    resource_changed_or_removed, App, IntoSystemConfigs, Plugin, PostStartup, State, Update,
+};
 
 pub(crate) struct ManualPlugin;
 
 impl Plugin for ManualPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, (spawn_manual, create_manual_key_bindings));
+        app.add_systems(
+            PostStartup,
+            (
+                (spawn_manual, update_manual).chain(),
+                create_manual_key_bindings,
+            ),
+        );
 
         app.add_systems(
             Update,
-            update_manual, // TODO run_if
+            update_manual.run_if(resource_changed_or_removed::<State<GameplayScreenState>>()),
         );
     }
 }
