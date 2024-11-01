93a0c345b905e42f20bbb2c6f4d39ad6b6d960a0
papow65 <papow65@protonmail.com>
2024-11-01 16:25:13 +0100

    Moved more types to gameplay/cdda/

--- src/gameplay/common/mod.rs
+++ src/gameplay/common/mod.rs
@@ -5,13 +5,9 @@ mod instruction;
 mod item;
 mod limited;
 mod local_terrain;
-mod mesh;
-mod model;
 mod nbor;
-mod object_definition;
 mod offset;
 mod region;
-mod type_id;
 mod vision_distance;
 mod walking_cost;
 
@@ -22,21 +18,11 @@ pub(crate) use self::instruction::*;
 pub(crate) use self::item::{Item, ItemItem};
 pub(crate) use self::limited::{Evolution, Limited};
 pub(crate) use self::local_terrain::LocalTerrain;
-pub(crate) use self::mesh::MeshInfo;
-pub(crate) use self::model::{Layers, Model, ModelShape, SpriteOrientation, Transform2d};
 pub(crate) use self::nbor::{CardinalDirection, HorizontalDirection, Nbor, NborDistance};
-pub(crate) use self::object_definition::ObjectCategory;
 pub(crate) use self::offset::{LevelOffset, PosOffset};
 pub(crate) use self::region::{Region, ZoneRegion};
-pub(crate) use self::type_id::TypeId;
 pub(crate) use self::vision_distance::VisionDistance;
 pub(crate) use self::walking_cost::WalkingCost;
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub(crate) enum SpriteLayer {
-    Front,
-    Back,
-}
 
 /// Visible to the player character
 #[derive(Clone, Debug, PartialEq, Eq)]
