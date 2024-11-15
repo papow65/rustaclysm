fb612512164c56fbbac30bdf9dc1a512d3d04bd9
papow65 <papow65@protonmail.com>
2024-11-15 06:15:37 +0100

    Better description for money on cash cards

--- src/gameplay/item/query_data.rs
+++ src/gameplay/item/query_data.rs
@@ -2,8 +2,11 @@ use crate::gameplay::{
     Amount, Containable, Filthy, Fragment, ItemIntegrity, ObjectDefinition, ObjectName, Pos,
     Positioning,
 };
+use crate::hud::text_color_expect_half;
 use bevy::ecs::query::QueryData;
-use bevy::prelude::{Entity, Parent};
+use bevy::prelude::{ops::atan2, Entity, Parent};
+use cdda_json_files::ObjectId;
+use std::f32::consts::FRAC_1_PI;
 
 #[derive(QueryData)]
 #[query_data(derive(Debug))]
@@ -21,15 +24,30 @@ pub(crate) struct Item {
 
 impl<'a> ItemItem<'a> {
     pub(crate) fn fragments(&self) -> impl Iterator<Item = Fragment> + use<'_, 'a> {
-        [
-            self.amount.fragment(),
-            self.filthy.map(|_| Filthy::fragment()),
-            self.integrity.fragment(),
-            Some(self.name.amount(self.amount.0, Pos::ORIGIN)),
-        ]
-        .into_iter()
-        .flatten()
-        .map(|mut fragment| {
+        let fragments = if self.definition.id == ObjectId::new("money") {
+            let cents = self.amount.0 as f32;
+            let dollars = format!("$ {:.2}", cents / 100.0);
+
+            // $ 100 is treated as the expected amount
+            let score = 2.0 * FRAC_1_PI * atan2(cents, 10000.0);
+            let color = text_color_expect_half(score);
+
+            [
+                None,
+                self.filthy.map(|_| Filthy::fragment()),
+                self.integrity.fragment(),
+                Some(Fragment::colorized(dollars, color)),
+            ]
+        } else {
+            [
+                self.amount.fragment(),
+                self.filthy.map(|_| Filthy::fragment()),
+                self.integrity.fragment(),
+                Some(self.name.amount(self.amount.0, Pos::ORIGIN)),
+            ]
+        };
+
+        fragments.into_iter().flatten().map(|mut fragment| {
             fragment.positioning = if let Some(&pos) = self.pos {
                 Positioning::Pos(pos)
             } else {
