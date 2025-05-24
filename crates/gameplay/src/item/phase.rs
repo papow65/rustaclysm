eea8e4e3b8e0888a77d295fb45e38f276f456fa2
papow65 <papow65@protonmail.com>
2025-05-24 03:55:04 +0200

    Added missing file

---
+++ crates/gameplay/src/item/phase.rs
@@ -0,0 +1,32 @@
+use crate::Fragment;
+use bevy::prelude::Component;
+use cdda_json_files::CddaPhase;
+use hud::{BLUE_TEXT_COLOR, WARN_TEXT_COLOR};
+
+#[derive(Copy, Clone, Debug, Component)]
+#[component(immutable)]
+pub(crate) enum Phase {
+    Solid,
+    Liquid,
+    Gas,
+}
+
+impl Phase {
+    pub(crate) fn suffix(self) -> Option<Fragment> {
+        match self {
+            Self::Solid => None,
+            Self::Liquid => Some(Fragment::colorized("(liquid)", BLUE_TEXT_COLOR)),
+            Self::Gas => Some(Fragment::colorized("(gas)", WARN_TEXT_COLOR)),
+        }
+    }
+}
+
+impl From<&CddaPhase> for Phase {
+    fn from(source: &CddaPhase) -> Self {
+        match source {
+            CddaPhase::Solid => Self::Solid,
+            CddaPhase::Liquid => Self::Liquid,
+            CddaPhase::Gas => Self::Gas,
+        }
+    }
+}
