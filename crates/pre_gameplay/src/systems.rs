98dd90861024f953fc231e32c8c6ced452624ae1
papow65 <papow65@protonmail.com>
2025-10-22 08:28:03 +0200

    Removed unnecessary dependency between two workspace crates

--- crates/pre_gameplay/src/systems.rs
+++ crates/pre_gameplay/src/systems.rs
@@ -1,16 +1,13 @@
 use application_state::ApplicationState;
 use bevy::prelude::{NextState, ResMut};
 use gameplay::GameplayReadiness;
-use gameplay_transition_state::GameplayTransitionState;
 
 #[expect(clippy::needless_pass_by_value)]
 pub(super) fn start_gameplay(
     mut next_application_state: ResMut<NextState<ApplicationState>>,
-    mut next_gameplay_transition_state: ResMut<NextState<GameplayTransitionState>>,
     gameplay_readiness: GameplayReadiness,
 ) {
     if gameplay_readiness.ready_to_load() {
         next_application_state.set(ApplicationState::Gameplay);
-        next_gameplay_transition_state.set(GameplayTransitionState::Loading);
     }
 }
